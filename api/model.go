61d68d98bf1f4f2c5c0cad940b248c6d0b9240b0
Danil Ivanov <danil.ivanov@epfl.ch>
2018-11-24 16:23:33 +0100

    db fix

--- api/model.go
+++ api/model.go
@@ -10,9 +10,10 @@ const (
 )
 
 type User struct {
-	ID      int     `bson:"_id"`
-	Name    string  `json:"username"`
-	Devices Devices `json:"devices"`
+	ID int `bson:"_id" json:"_id"`
+
+	Name    string  `bson:"username" json:"username"`
+	Devices Devices `bson:"devices" json:"devices"`
 }
 type Users []User
 
@@ -20,11 +21,11 @@ type Users []User
 type MAC string
 type IP string
 type Device struct {
-	ID      int        `bson:"_id"`
-	MACAddr MAC        `json:"mac_address"`
-	IPAddr  IP         `json:"ip_address"`
-	Name    string     `json:"device_name"`
-	Type    DeviceType `json:"device_type"`
-	Mobile  bool       `json:"mobile"`
+	ID      int        `bson:"_id" json:"_id"`
+	MACAddr MAC        `bson:"mac_address" json:"mac_address"`
+	IPAddr  IP         `bson:"ip_address" json:"ip_address"`
+	Name    string     `bson:"device_name" json:"device_name"`
+	Type    DeviceType `bson:"device_type" json:"device_type"`
+	Mobile  bool       `bson:"mobile" json:"mobile"`
 }
 type Devices []Device
