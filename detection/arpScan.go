8ed5a2443fdaaec3679b46fc74967ee36c84360c
Luigi A. A. Sansonetti <luigi@sansonetti.ch>
2018-11-24 19:07:57 +0100

    Okay, that's the final change, now I swear it works

--- detection/arpScan.go
+++ detection/arpScan.go
@@ -21,7 +21,7 @@ func Scan() (IPandMAC map[string]string) {
 	asStrings = asStrings[2 : len(asStrings)-4]
 	for _, line := range asStrings {
 		asArray := strings.Split(line, "\t")
-		IPandMAC[asArray[0]] = asArray[1]
+		IPandMAC[asArray[0]] = strings.Split(asArray[1], " ")[0]
 	}
 
 	return
