c498fae3c439f32eb3823f64121135345597ec18
Luigi A. A. Sansonetti <luigi@sansonetti.ch>
2018-11-24 19:04:48 +0100

    Redid the arp-scan. Now 100% working

--- main.go
+++ main.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+	"fmt"
 
-	"github.com/gorilla/handlers"
-	"github.com/kodefish/go-presence-detection/api"
 	"github.com/kodefish/go-presence-detection/detection"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	/*
+		port := os.Getenv("PORT")
+		if port == "" {
+			log.Fatal("$PORT must be set")
+		}
+
+		router := api.NewRouter() // create routes
+
+		allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+		allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+
+		// Launch server with CORS validation
+		log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(allowedOrigins, allowedMethods)(router)))
+	*/
+
+	for key, value := range detection.Scan() {
+		fmt.Printf("%s - %s\n", key, value)
 	}
-
-	router := api.NewRouter() // create routes
-
-	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
-	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
-
-	// Launch server with CORS validation
-	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(allowedOrigins, allowedMethods)(router)))
-
-	// Runs an arp scan, yay!
-	detection.Main()
-)
+}
